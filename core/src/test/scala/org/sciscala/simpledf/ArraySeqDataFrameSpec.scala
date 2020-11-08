34f3c1825b77ef9f8e3217528475049d88a83d36
Eric Rodriguez <eric.rodriguez@eniro.com>
2020-11-08 22:42:11 +0100

    Use simulacrum on tests

--- core/src/test/scala/org/sciscala/simpledf/ArraySeqDataFrameSpec.scala
+++ core/src/test/scala/org/sciscala/simpledf/ArraySeqDataFrameSpec.scala
@@ -74,126 +74,126 @@ class ArraySeqDataFrameSpec extends AnyFlatSpec with Matchers {
   )
 
   "Size" should "equal 12" in {
-    DataFrame[ArraySeqDataFrame].size(df) shouldEqual 12
+    df.size shouldEqual 12
   }
 
   "Shape" should "equal (6,2)" in {
-    DataFrame[ArraySeqDataFrame].shape(df) shouldBe (6, 2)
+    df.shape shouldBe (6, 2)
   }
 
   "Head" should "return a Dataframe with the first 5 rows" in {
-    val d = DataFrame[ArraySeqDataFrame].head(df)
+    val d = df.head()
     d.data shouldBe data.take(5)
     d.index shouldBe index.take(5)
     d.columns shouldBe cols.take(5)
   }
 
   "head(2)" should "return a Dataframe with the first 2 rows" in {
-    val d = DataFrame[ArraySeqDataFrame].head(df, 2)
+    val d = df.head(2)
     d.data shouldBe data.take(2)
     d.index shouldBe index.take(2)
     d.columns shouldBe cols.take(2)
   }
 
   "Head of dfNoIndex" should "return a Dataframe with the first 5 rows and an empty index" in {
-    val d = DataFrame[ArraySeqDataFrame].head(dfNoIndex)
+    val d = dfNoIndex.head()
     d.data shouldBe data.take(5)
     d.index shouldBe ArraySeq()
     d.columns shouldBe cols.take(5)
   }
 
   "Head of dfNoCols" should "return a Dataframe with the first 5 rows and a empty column names" in {
-    val d = DataFrame[ArraySeqDataFrame].head(dfNoCols)
+    val d = dfNoCols.head()
     d.data shouldBe data.take(5)
     d.index shouldBe index.take(5)
     d.columns shouldBe ArraySeq()
   }
 
   "Tail" should "return a Dataframe with the last 5 rows" in {
-    val d = DataFrame[ArraySeqDataFrame].tail(df)
+    val d = df.tail()
     d.data shouldBe data.takeRight(5)
     d.index shouldBe index.takeRight(5)
     d.columns shouldBe cols.takeRight(5)
   }
 
   "tail(2)" should "return a Dataframe with the last 2 rows" in {
-    val d = DataFrame[ArraySeqDataFrame].tail(df, 2)
+    val d = df.tail(2)
     d.data shouldBe data.takeRight(2)
     d.index shouldBe index.takeRight(2)
     d.columns shouldBe cols.takeRight(2)
   }
 
   "Tail of dfNoIndex" should "return a Dataframe with the last 5 rows and an empty index" in {
-    val d = DataFrame[ArraySeqDataFrame].tail(dfNoIndex)
+    val d = dfNoIndex.tail()
     d.data shouldBe data.takeRight(5)
     d.index shouldBe ArraySeq()
     d.columns shouldBe cols.takeRight(5)
   }
 
   "Tail of dfNoCols" should "return a Dataframe with the last 5 rows and a empty column names" in {
-    val d = DataFrame[ArraySeqDataFrame].tail(dfNoCols)
+    val d = dfNoCols.tail()
     d.data shouldBe data.takeRight(5)
     d.index shouldBe index.takeRight(5)
     d.columns shouldBe ArraySeq()
   }
 
   "iat(2,1)" should "equal 8" in {
-    DataFrame[ArraySeqDataFrame].iat(df, 2, 1) shouldBe Some(8)
+    df.iat(2, 1) shouldBe Some(8)
   }
 
   "iat(-2,1)" should "equal 14" in {
-    DataFrame[ArraySeqDataFrame].iat(df, -2, 1) shouldBe Some(14)
+    df.iat(-2, 1) shouldBe Some(14)
   }
 
   "iat(2,-1)" should "equal 7" in {
-    DataFrame[ArraySeqDataFrame].iat(df, 2, -1) shouldBe Some(8)
+    df.iat(2, -1) shouldBe Some(8)
   }
 
   "iat(-2,-1)" should "equal 13" in {
-    DataFrame[ArraySeqDataFrame].iat(df, -2, -2) shouldBe Some(13)
+    df.iat(-2, -2) shouldBe Some(13)
   }
 
   "at('viper', 'stamina')" should "equal 2" in {
-    DataFrame[ArraySeqDataFrame].at(df, "viper", "stamina") shouldBe Some(2)
+    df.at("viper", "stamina") shouldBe Some(2)
   }
 
   "at('viper', 'venom')" should "be None" in {
-    DataFrame[ArraySeqDataFrame].at(df, "viper", "venom") shouldBe None
+    df.at("viper", "venom") shouldBe None
   }
 
   "loc('viper')" should "return df's first row" in {
-    DataFrame[ArraySeqDataFrame].loc(df, "viper") shouldBe
+    df.loc("viper") shouldBe
     ArraySeqDataFrame(ArraySeq(ArraySeq(1), ArraySeq(2)), ArraySeq("viper"), cols)
   }
 
   "loc('venom')" should "return 'null' dataframe" in {
-    DataFrame[ArraySeqDataFrame].loc(df, "venom") shouldBe nullArraySeqDF
+    df.loc("venom") shouldBe nullArraySeqDF
   }
 
   "loc('viper', 'python')" should "return first and fourth rows" in {
-    DataFrame[ArraySeqDataFrame].loc(df, Seq("viper", "python")) shouldBe
+    df.loc(Seq("viper", "python")) shouldBe
     ArraySeqDataFrame(ArraySeq(ArraySeq(1, 10), ArraySeq(2, 11)), ArraySeq("viper", "python"), cols)
   }
 
   "loc('viper', 'venom')" should "return a dataframe with only 'viper' elements" in {
-    DataFrame[ArraySeqDataFrame].loc(df, Seq("viper", "venom")) shouldBe
+    df.loc(Seq("viper", "venom")) shouldBe
     ArraySeqDataFrame(ArraySeq(ArraySeq(1), ArraySeq(2)), ArraySeq("viper"), cols)
   }
 
   "loc(true, false, false, true, false, false)" should "return first and fourth rows" in {
-    DataFrame[ArraySeqDataFrame]
-      .loc(df, Seq(true, false, false, true, false, false)) shouldBe
+    df
+      .loc(Seq(true, false, false, true, false, false)) shouldBe
       ArraySeqDataFrame(ArraySeq(ArraySeq(1, 10), ArraySeq(2, 11)), ArraySeq("viper", "python"), cols)
   }
 
   "loc(false, false, false, false, false, false)" should "return a dataframe with no rows and only colNames" in {
-    DataFrame[ArraySeqDataFrame]
-      .loc(df, Seq(false, false, false, false, false, false)) shouldBe dfOnlyCols
+    df
+      .loc(Seq(false, false, false, false, false, false)) shouldBe dfOnlyCols
   }
 
   "loc(true, true, true, true, true, true)" should "be the original dataframe" in {
-    DataFrame[ArraySeqDataFrame]
-      .loc(df, Seq(true, true, true, true, true, true)) shouldBe df
+    df
+      .loc(Seq(true, true, true, true, true, true)) shouldBe df
   }
 
   "Encoder" should "encode DataFrame" in {
@@ -205,33 +205,34 @@ class ArraySeqDataFrameSpec extends AnyFlatSpec with Matchers {
   }
 
   "to" should "encode DataFrame" in {
-    DataFrame[ArraySeqDataFrame].to(df) shouldBe serpents
+    df.to shouldBe serpents
   }
 
   "to" should "encode DataFrame with no index" in {
-    DataFrame[ArraySeqDataFrame].to(dfNoIndex) shouldBe serpentsNoIndex
+    dfNoIndex.to shouldBe serpentsNoIndex
   }
 
   "insert" should "adds a column at index `loc`" in {
     val newCol = ArraySeq(13,15,17,19,11,20)
-    val newDF = DataFrame[ArraySeqDataFrame].insert[Column[Int]](df, 1, "sight", newCol, false).getOrElse(nullArraySeqDF)
+    val newDF = df.insert[Column[Int]](1, "sight", newCol, false).getOrElse(nullArraySeqDF)
     newDF.data(1) shouldBe newCol
   }
 
   "empty" should "return true if dataframe is empty" in {
-    DataFrame[ArraySeqDataFrame].empty(nullArraySeqDF) shouldBe true
+    nullArraySeqDF.empty shouldBe true
   }
 
   "empty" should "return false if dataframe is not empty" in {
-    DataFrame[ArraySeqDataFrame].empty(df) shouldBe false
+    df.empty shouldBe false
   }
 
   "get(name)" should "return the dataframe with the `name` label" in {
-    DataFrame[ArraySeqDataFrame].get[String](df, "speed", None) shouldBe ArraySeqDataFrame(ArraySeq(ArraySeq(1,4,7,10,13,16)), index, ArraySeq("speed"))
+    //df.get[String](df, "speed", None) shouldBe ArraySeqDataFrame(ArraySeq(ArraySeq(1,4,7,10,13,16)), index, ArraySeq("speed"))
+    df.get[String]("speed", None) shouldBe ArraySeqDataFrame(ArraySeq(ArraySeq(1,4,7,10,13,16)), index, ArraySeq("speed"))
   }
 
   "get(unknown)" should "return the default value" in {
-    DataFrame[ArraySeqDataFrame].get[String](df, "poison", None) shouldBe nullArraySeqDF
+    df.get[String]("poison", None) shouldBe nullArraySeqDF
   }
 
   "items" should "return data in Array[(columnName, Column)] format" in {
