02c822de6182fa041c8859701a286736dd26873f
Eric Rodriguez <eric.rodriguez@eniro.com>
2021-01-28 00:40:05 +0100

    Json parser working, TBD read file

--- core/src/main/scala/org/sciscala/simpledf/arrayseq/Json2ArraySeqDataFrame.scala
+++ core/src/main/scala/org/sciscala/simpledf/arrayseq/Json2ArraySeqDataFrame.scala
@@ -3,9 +3,8 @@ package org.sciscala.simpledf.arrayseq
 import ujson.Value.Value
 import ujson.read
 
-import scala.collection.generic.DefaultSerializable
-import scala.collection.mutable.ArrayBuffer
-import scala.collection.{AbstractIterable, StrictOptimizedIterableOps, immutable, mutable}
+import scala.collection.immutable.ArraySeq
+import scala.collection.mutable.{ArrayBuffer, LinkedHashMap => MLinkedHashMap, ArraySeq => MArraySeq}
 
 object Json2ArraySeqDataFrame extends App {
 
@@ -16,64 +15,75 @@ object Json2ArraySeqDataFrame extends App {
     """{"base": "USD","date": "2021-01-24","time_last_updated": 1611446401,"rates": {"USD": 1,"AED": 3.6725,"ARS": 86.363457,"AUD": 1.294731,"BGN": 1.606893,"BRL": 5.383967,"BSD": 1,"CAD": 1.268251,"CHF": 0.885517,"CLP": 717.616192,"CNY": 6.478288,"COP": 3471.118897,"CZK": 21.451045}}""".stripMargin
 
   def processObj(
-      obj: mutable.LinkedHashMap[String, ujson.Value.Value],
+      obj: MLinkedHashMap[String, ujson.Value.Value]
   ): ArraySeqDataFrame = {
-    val indexCol = obj.find(t => t._2.getClass.getName == "ujson.Obj")
+    var df: MArraySeq[MArraySeq[_]] =
+      MArraySeq.empty[MArraySeq[_]]
+    val src = obj.zipWithIndex
+    val indexCol: Option[(String, Value)] =
+      obj.find(t => t._2.getClass.getName == "ujson.Obj")
+
+    def fillData(
+        cell: ((String, Value), Int)
+    ): Unit = {
+      val idx = cell._2
+      if (!df.isDefinedAt(idx)) {
+        df = df.appended(MArraySeq.empty[cell._1._2.type])
+      }
+      df.update(idx, df(idx).appended(cell._1._2.value))
+    }
+
     indexCol match {
       case None => {
-        var df: mutable.ArraySeq[mutable.ArraySeq[_]] =
-          mutable.ArraySeq.empty[mutable.ArraySeq[_]]
-
-        val src = obj.zipWithIndex
-
-        var cols: immutable.ArraySeq[String] =
-          immutable.ArraySeq.from(src.map(_._1._1))
-
-        src.foreach(tup => {
-          val idx = tup._2
-          if (!df.isDefinedAt(idx)) {
-            df = df.appended(mutable.ArraySeq.empty[tup._1._2.type])
-          }
-          df.update(idx, df(idx).appended(tup._1._2.value))
-        })
-
-        val data = immutable.ArraySeq.from(
-          df.map(arr => immutable.ArraySeq.from(arr.toIterable))
+        var cols: ArraySeq[String] =
+          ArraySeq.from(src.map(_._1._1))
+        src.foreach(fillData)
+        val data = ArraySeq.from(
+          df.map(arr => ArraySeq.from(arr.toIterable))
         )
-
-        ArraySeqDataFrame(data, immutable.ArraySeq.empty[String], cols)
+        ArraySeqDataFrame(data, ArraySeq.empty[String], cols)
       }
-      case Some(_) => ArraySeqDataFrame.emptyInstance
+      case Some((key, value)) => {
+        var cols: ArraySeq[String] =
+          ArraySeq.from(src.map(_._1._1).filter(_ != key))
+        val lm = value.value
+          .asInstanceOf[MLinkedHashMap[String, ujson.Value.Value]]
+        val index = ArraySeq.from(lm.keySet)
+        val indexValues = ArraySeq.from(lm.values)
+        src.filter(tup => tup._1._1 != key).foreach(fillData)
+        val data = ArraySeq.from(
+          indexValues +: df.map(arr => ArraySeq.from(Seq.fill(index.size)(arr(0))))
+        )
+        ArraySeqDataFrame(data, index, key +: cols)
+      }
     }
   }
 
   def processArr(
-      arr: mutable.ArrayBuffer[ujson.Obj]
+      arr: ArrayBuffer[ujson.Obj]
   ): ArraySeqDataFrame = {
-    var df: mutable.ArraySeq[mutable.ArraySeq[_]] =
-      mutable.ArraySeq.empty[mutable.ArraySeq[_]]
+    var df: MArraySeq[MArraySeq[_]] =
+      MArraySeq.empty[MArraySeq[_]]
 
-    val lst = arr(0)//.value
-    var cols: immutable.ArraySeq[String] =
-      immutable.ArraySeq.from(lst.value.zipWithIndex.map(_._1._1))
+    val lst = arr(0) //.value
+    var cols: ArraySeq[String] =
+      ArraySeq.from(lst.value.zipWithIndex.map(_._1._1))
 
-    arr.foreach(obj => {
-      val src = obj.value.zipWithIndex
-
-      src.foreach(tup => {
+    arr.foreach(obj =>
+      obj.value.zipWithIndex.foreach(tup => {
         val idx = tup._2
         if (!df.isDefinedAt(idx)) {
-          df = df.appended(mutable.ArraySeq.empty[tup._1._2.type])
+          df = df.appended(MArraySeq.empty[tup._1._2.type])
         }
         df.update(idx, df(idx).appended(tup._1._2.value))
       })
-    })
-
-    val data = immutable.ArraySeq.from(
-      df.map(arr => immutable.ArraySeq.from(arr.toIterable))
     )
 
-    ArraySeqDataFrame(data, immutable.ArraySeq.empty[String], cols)
+    val data = ArraySeq.from(
+      df.map(arr => ArraySeq.from(arr.toIterable))
+    )
+
+    ArraySeqDataFrame(data, ArraySeq.empty[String], cols)
   }
 
   def processJsonString(jsonString: String): ArraySeqDataFrame =
@@ -82,7 +92,7 @@ object Json2ArraySeqDataFrame extends App {
         processArr(value.asInstanceOf[ArrayBuffer[ujson.Obj]])
       case ujson.Obj(value) =>
         processObj(
-          value.asInstanceOf[mutable.LinkedHashMap[String, ujson.Value.Value]]
+          value.asInstanceOf[MLinkedHashMap[String, ujson.Value.Value]]
         )
     }
 
